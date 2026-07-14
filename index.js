0902043dbad4618d4806d4f90f0966b3c013554f
altaks77 <altaks77@gmail.com>
2026-07-14 21:07:33 +0700

    draft4

--- index.js
+++ index.js
@@ -56,8 +56,9 @@ async function menu() {
    };
 
 async function jawa() {
-  let b = await rl.question("Masukan pin anda..\n")
-  if (b == card.pw) {
+  let pinInput = await rl.question("Masukan pin anda..\n");
+  let pinType = Number(pinInput);
+  if (pinType === card.pw) {
     console.log("Pin benar\n Selamat datang!");
     console.log(`nama : ${card.nama} \n id card : ${card.id} \n saldo : ${card.saldo} \n Utang : ${card.utang}`);
     if (card.utang > card.saldo) {
@@ -85,17 +86,3 @@ else {
   console.clear
   rl.close()
 };
-
-// masih follow
-// gw mencet apaan ampe ngefollow begini, coba run lagi rip qi, i mau main, hehe
-// eror syid, di bagian tarik ada await
-// async fucntion (choose)ny coba diapain, run lagi coba
-// pas bagian setor ato tariknya, dia kayak mandet gitu jadi kyk bisa ngespam jawaban. apa gr2 sistem async sama awaitnya ya.
-// coba g pake bisa g
-// malah eror di bagian isNaN nya. bingung suer dah, coba run rip qi gimme the runing thing
-//udh tu
-// mengapa begini ;-
-//coba tanya sajat:)
-//yey
-//msh follow ya apa udh ngga, follow aoa bobo yu, bole, besok tanya kk ny
-//knjewqknqkwjnekneqwjkenjnwekjnwknjjnkjnkjnkjn
